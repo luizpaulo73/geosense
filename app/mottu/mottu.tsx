50269202d8e7c72b86f2dce82d7512288e3a5a48
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-05-21 11:57:50 -0300

    change: reestilizacao da pagina mottu

--- app/mottu/mottu.tsx
+++ app/mottu/mottu.tsx
@@ -8,11 +8,14 @@ const { width } = Dimensions.get("window");
 
 export default function mottu() {
     return (
-        <BaseTelas titulo="Mottu Sport" botaoVoltar="/motos">
-            <Image
-                source={require("../../assets/motos/mottu-sport.png")}
-                style={style.image}
-            />
+        <BaseTelas titulo="" botaoVoltar="/motos">
+            <View style={{justifyContent: "center", alignItems: "center"}}>
+                <Text style={[style.title, {marginBottom: -100}]}>Mottu Sport</Text>
+                <Image
+                    source={require("../../assets/motos/mottu-sport.png")}
+                    style={style.image}
+                />
+            </View>
             <View style={{gap: 20, width: "100%", alignItems: "center"}}>
                 <InfoMoto />
                 <InfoDatasMoto />
@@ -22,6 +25,12 @@ export default function mottu() {
 }
 
 const style = StyleSheet.create({
+    title: {
+        fontSize: 50,
+        color: "#fff",
+        width: "90%",
+        fontFamily: "K2D_700Bold",
+    },
     image: {
         width: width / 1.1,
         height: width / 1.1,
