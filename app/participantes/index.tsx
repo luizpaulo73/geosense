e8cf0bda766c92f6cb5c24ea2b7f1a6bd4092f56
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-05-21 08:46:44 -0300

    feat: dados dos participantes adicionados

---
+++ app/participantes/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import BaseTelas from "../../components/BaseTelas/BaseTelas";
+
+export default function participantes() {
+    return (
+        <BaseTelas titulo="Participantes">
+            <Text>ofdughsduighi</Text>
+        </BaseTelas>
+    )
+}
