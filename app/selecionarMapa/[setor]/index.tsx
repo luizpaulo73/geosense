7de614bb3ce13ca802b72768025de98a1b0a5503
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-05-21 11:35:07 -0300

    feat: estilizacao da tela de selecionar no mapa

--- app/selecionarMapa/[setor]/index.tsx
+++ app/selecionarMapa/[setor]/index.tsx
@@ -1,6 +1,7 @@
 import Setor from '../../../components/Setor/Setor';
-import { useLocalSearchParams } from 'expo-router';
+import { Link, useLocalSearchParams } from 'expo-router';
 import BaseTelas from '../../../components/BaseTelas/BaseTelas';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function SelecionarMapa() {
 
@@ -8,8 +9,36 @@ export default function SelecionarMapa() {
     const setorString = Array.isArray(setor) ? setor[0] : setor || '';
 
     return (
-        <BaseTelas titulo='' botaoVoltar="/cadastro">
+        <BaseTelas titulo='Selecione a Vaga' botaoVoltar="/cadastro">
+            <View style={style.container}>
             <Setor area={setorString}/>
+            <Link href="/dashboard" style={style.btnLogin}>
+                <Text style={style.btnLoginText}>Cadastrar</Text>
+            </Link></View>
         </BaseTelas>
     )
 }
+
+const style = StyleSheet.create({
+    container : {
+        flex: 1,
+        alignItems: "center",
+        width: "100%",
+        marginTop: 20
+    },
+    btnLogin: {
+        backgroundColor: "#00CCCF",
+        borderRadius: 5,
+        width: "50%",
+        height: 40,
+        marginTop: 20,
+    },
+    btnLoginText: {
+        flex: 1,
+        color: "#000",
+        fontSize: 20,
+        textAlign: "center",
+        fontFamily: "K2D_700Bold",
+        textAlignVertical: "center",
+    }
+})
