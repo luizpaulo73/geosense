f2d67175d467cc344d6652bfc8178cb663b94f06
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-09-18 10:39:31 -0300

    fix: add import to the font k2d

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,17 @@
 import { Slot } from "expo-router";
 import { ThemeProvider } from "../context/ThemeContext";
+import { useFonts, K2D_400Regular, K2D_700Bold } from "@expo-google-fonts/k2d"
 
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+        K2D_400Regular,
+        K2D_700Bold,
+    });
+      
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <ThemeProvider>
             <Slot />
